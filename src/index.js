8baa2aac3a04c96844f65194f67fcb02bf68e184
DrVonDevious <drvondevious@gmail.com>
2020-03-12 07:19:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+const btn = document.querySelector("button")
+
+function createUser(json) {
+  const labels = Array.from(document.querySelectorAll("h4"))
+
+  const data = json.map( info => {
+    Object.values(info).map( key => tag.innerText = key );
+  });
+
+  labels.map( tag => {
+  });
+
+};
+
+btn.addEventListener("click", () => {
+  fetch('https://randomuser.me/api/')
+    .then( response => {
+      return response.json()
+    })
+    .then( json => {
+      console.log(json['results'])
+      createUser(json["results"])
+    });
+});
